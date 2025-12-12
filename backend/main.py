6a0c60507ca6e5bf4f1783b0d842deb936e270d7
Артем <mrsikkel@gmail.com>
2025-12-12 23:44:44 +0700

    Привязка front+back демоверсия

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
 from database import engine, Base
 from routers import auth, users, products, articles
+from fastapi.staticfiles import StaticFiles
+import os
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.mount(
+    "/static",
+    StaticFiles(directory=os.path.join(os.path.dirname(__file__), "../frontend")),
+    name="static"
+)
 
 app.include_router(auth.router)
 app.include_router(users.router)
