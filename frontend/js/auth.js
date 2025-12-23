60b25afeaad3609d26ce86f6509a027aea9dc671
Rashchinskiy <rasiskija@gmail.com>
2025-12-23 18:00:27 +0700

    fix backend-part auth

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
         try {
-            const response = await fetch("http://127.0.0.1:8000/auth/login", {
+            const response = await fetch("http://127.0.0.1:8000/auth/login-json", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ email, password })
